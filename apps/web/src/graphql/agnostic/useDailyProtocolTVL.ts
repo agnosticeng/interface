e9ccab198e81dfd12dd55bfd55ec79b1d5abe5db
Yann Amsellem <yann@agnostic.engineering>
2024-05-07 16:54:50 +0200

    chore(explore): use agnostic Historical Protocol TVL query

---
+++ apps/web/src/graphql/agnostic/useDailyProtocolTVL.ts
@@ -0,0 +1,47 @@
+import { useQuery } from '@apollo/client'
+import { gql } from 'graphql-tag'
+import { Chain } from 'graphql/data/__generated__/types-and-hooks'
+import { useMemo } from 'react'
+import { client } from './client'
+
+const HISTORICAL_PROTOCOL_TVL_QUERY = gql`
+  query HistoricalProtocolTVL {
+    explore_daily_protocol_tvl_v3 {
+      timestamp
+      tvl_usd
+    }
+  }
+`
+
+type AgnosticHistoricalProtocolTVLResult = {
+  explore_daily_protocol_tvl_v3?: { timestamp: string; tvl_usd: string }[]
+}
+
+// eslint-disable-next-line import/no-unused-modules
+export function useDailyProtocolTvlQuery({ variables }: { variables: { chain: Chain } }) {
+  const { data, loading } = useQuery<AgnosticHistoricalProtocolTVLResult>(HISTORICAL_PROTOCOL_TVL_QUERY, {
+    client,
+    skip: variables.chain !== Chain.Ethereum,
+  })
+
+  return {
+    data: useMemo(
+      () => ({ v3DailyProtocolTvl: data?.explore_daily_protocol_tvl_v3?.map(mapper), v2DailyProtocolTvl: undefined }),
+      [data]
+    ),
+    loading,
+  }
+}
+
+function mapper(row: { timestamp: string; tvl_usd: string }): {
+  readonly __typename?: 'TimestampedAmount'
+  readonly id: string
+  readonly timestamp: number
+  readonly value: number
+} {
+  return {
+    id: window.btoa(`TimestampedAmount:${row.timestamp}_${row.tvl_usd}`),
+    timestamp: parseInt(row.timestamp, 10),
+    value: parseFloat(row.tvl_usd),
+  }
+}
--- apps/web/src/graphql/data/protocolStats.ts
+++ apps/web/src/graphql/data/protocolStats.ts
@@ -3,6 +3,7 @@ import { StackedHistogramData } from 'components/Charts/VolumeChart/renderer'
 import { ChartType } from 'components/Charts/utils'
 import { ChartQueryResult, checkDataQuality } from 'components/Tokens/TokenDetails/ChartSection/util'
 import gql from 'graphql-tag'
+import { useDailyProtocolTvlQuery } from 'graphql/agnostic/useDailyProtocolTVL'
 import { useHistoricalProtocolVolumeQuery } from 'graphql/agnostic/useHistoricalProtocolVolume'
 import {
   Chain,
@@ -10,7 +11,6 @@ import {
   PriceSource,
   ProtocolVersion,
   TimestampedAmount,
-  useDailyProtocolTvlQuery,
 } from 'graphql/data/__generated__/types-and-hooks'
 import { UTCTimestamp } from 'lightweight-charts'
 import { useMemo } from 'react'
